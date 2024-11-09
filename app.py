ce10209408525bd814007752fb9e1ccb855127ca
Expooo7 <berevoescu.remus@gmail.com>
2024-11-09 17:13:43 +0200

    streamlit app

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+
+# Function to create the slidable menu
+def slidable_menu():
+    # Inject custom CSS to style the sidebar
+    st.markdown(
+        """
+        <style>
+        [data-testid="stSidebar"] {
+            background-color: yellow;
+            border: 1px solid black;
+            color: black;
+        }
+        [data-testid="stSidebar"] .css-1d391kg {
+            color: black;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+    
+    with st.sidebar:
+        st.title("Right-Hand Menu")
+        st.write("This is a slidable right-hand menu.")
+        
+        main_option = st.selectbox("Choose a main option", ["A", "B", "C", "D"])
+        
+        if main_option == "A":
+            sub_option = st.selectbox("Choose a sub-option for A", ["A1", "A2", "A3"])
+        elif main_option == "B":
+            sub_option = st.selectbox("Choose a sub-option for B", ["B1", "B2"])
+        elif main_option == "C":
+            sub_option = st.selectbox("Choose a sub-option for C", ["C1", "C2", "C3", "C4"])
+        elif main_option == "D":
+            sub_option = st.selectbox("Choose a sub-option for D", ["D1", "D2", "D3", "D4", "D5"])
+        
+        st.button("Click me")
+
+# Main function to run the Streamlit app
+def main():
+    st.title("Main Page")
+    st.write("This is the main content of the page.")
+    slidable_menu()
+
+if __name__ == "__main__":
+    main()
